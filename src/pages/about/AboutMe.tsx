c1cdd3620a0e52009ec36728bd4f4bdccb2ec23e
saewoohan <112225610+saewoohan@users.noreply.github.com>
2024-06-30 13:21:57 +0900

    fix: about me 수정

--- src/pages/about/AboutMe.tsx
+++ src/pages/about/AboutMe.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
+import { HighlightText } from '../../common/components/HightlightSpan'
 
 export const AboutMe = () => {
   const personalInfo = [
     { label: '이름', value: '한승우' },
-    { label: '생년월일', value: '2000년 12월 23일' },
-    { label: '주소지', value: '서울특별시 성동구' },
     { label: '이메일', value: 'hsoooo2341@gamil.com' },
     { label: '연락처', value: '+82-10-7299-2341' },
     { label: '학력', value: '한양대학교 컴퓨터소프트웨어학부 (4학년 재학)' },
@@ -22,10 +20,25 @@ export const AboutMe = () => {
           <div className="text-2xl sm:text-4xl mb-4 font-extrabold">
             About me 🙂
           </div>
-          <div className="text-xs sm:text-base mb-4 font-regular border-l-4 border-blue-500 pl-4">
-            {
-              '저는 한번 시작한 일은 끝을 봐야 직성이 풀리는 개발자 한승우입니다. 끈기와 열정을 가지고 항상 새로운 것을 배우고자 노력하며, 현실에 안주하지 않고 끊임없이 성장하는 개발자가 되기 위해 노력하고 있습니다.'
-            }
+          <div className="text-sm sm:text-base mb-4 font-regular border-l-4 border-blue-500 pl-4 flex flex-col space-y-1">
+            <HighlightText
+              text="🤔 끈기와 열정을 가지고 항상 새로운 것을 배우고자 노력하며, 현재에
+              안주하지 않고 끊임없이 성장하는 개발자가 되기 위해 노력하고
+              있습니다."
+              highlights={['성장', '끈기', '열정']}
+            />
+            <HighlightText
+              text="📞 다양한 사람들과 소통하는 것을 즐기며, 팀원들과 협업하여 더 나은 결과물을 만들어내는 것을 좋아합니다."
+              highlights={['소통', '협업']}
+            />
+            <HighlightText
+              text="🤝 한번 시작한 일은 끝까지 완수하는 것을 중요하게 생각하며, 문제가 발생했을 때 빠르게 대처하여 해결하는 것을 좋아합니다."
+              highlights={['끝까지', '해결']}
+            />
+            <HighlightText
+              text="🤯 개발이라는 것은 디지털 매체를 통한 일종의 창작활동이라고 생각합니다. 단순히 문제를 해결하는 개발자가 아닌 개발자로서의 주관과 직관을 기르기위해 노력하고 있습니다."
+              highlights={['창작활동', '주관', '직관']}
+            />
           </div>
           <ul className="w-full">
             {personalInfo.map((info) => (
